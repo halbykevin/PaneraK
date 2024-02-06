0a72b7cf5bb2b3591559ca4a4cfc1251322c4a85
halbykevin <halbykevin@hotmail.com>
2024-02-07 01:43:04 +0200

    more smart approach to change the language

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 document.addEventListener('DOMContentLoaded', function () {
     var langToggle = document.getElementById('lang-toggle');
-    var currentLanguage = document.documentElement.lang; // Get the current language from the <html> tag
+    var currentLang = document.documentElement.lang; // Get the current page language
+    var pathname = window.location.pathname;
+    var pageName = pathname.split("/").pop(); // Gets the current file name, e.g., 'pizza.html'
   
-    // Set the checkbox state based on the language
-    langToggle.checked = (currentLanguage === 'ar');
-  
-    langToggle.addEventListener('change', function () {
-      if (this.checked) {
-        // Redirect to the Arabic version of the site
-        window.location.href = 'ArIndex.html';
+    // Function to derive the URL for the other language version of the page
+    function getOtherLanguageUrl(currentLang, pageName) {
+      var baseName = pageName.split('.')[0]; // Gets the 'pizza' from 'pizza.html'
+      var ext = pageName.split('.')[1]; // Gets the 'html' from 'pizza.html'
+      if (currentLang === 'ar') {
+        // If current language is Arabic, strip '-ar' from the name to get the English version
+        return baseName.replace('-ar', '') + '.' + ext;
       } else {
-        // Redirect to the English version of the site
-        window.location.href = 'index.html';
+        // If current language is English, add '-ar' to get the Arabic version
+        return baseName + '-ar.' + ext;
       }
+    }
+  
+    // Set the toggle switch position based on the current language
+    langToggle.checked = (currentLang === 'ar');
+  
+    // Event listener for the toggle switch
+    langToggle.addEventListener('change', function () {
+      window.location.href = getOtherLanguageUrl(currentLang, pageName);
     });
-  }); 
+  });
+  
